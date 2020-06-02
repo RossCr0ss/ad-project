92f25cd01e58993ed16c83a1c88ddc23f9016ec0
RossCr0ss <sobechko.r@gmail.com>
2020-06-02 19:38:44 +0300

    add Appbar

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,40 @@
 <template>
-  <h1>Hello World</h1>
+    <v-app>
+        <v-navigation-drawer app temporary>
+            <!-- -->
+        </v-navigation-drawer>
+
+        <v-app-bar app>
+            <!-- -->
+        </v-app-bar>
+
+        <!-- Sizes your content based upon application components -->
+        <v-content>
+
+            <!-- Provides the application the proper gutter -->
+            <v-container fluid>
+
+            <!-- If using vue-router -->
+            <router-view></router-view>
+            </v-container>
+        </v-content>
+
+        <v-footer app>
+            <!-- -->
+        </v-footer>
+    </v-app>
 </template>
 
 <script>
+import AppBar from './components/AppBar.vue'
+export default {
+    data() {
+        return {
 
-export default {}
+        }
+    },
+    components: {
+        'v-app-bar': AppBar
+    }
+}
 </script>
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
+import '@fortawesome/fontawesome-free/css/all.css'
+import '@mdi/font/css/materialdesignicons.css'
 
 Vue.use(Vuetify)
 
 export default new Vuetify({
+    icons: {
+        iconfont: 'mdi'
+    },
 })
