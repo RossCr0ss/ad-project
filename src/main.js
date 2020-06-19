3b4d6039edff2b31db1cd6ff0e292580c4526dac
Rostyslav <sobechko@newveer.com>
2020-06-19 11:14:54 +0300

    change structure of project

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import vuetify from './plugins/vuetify'
-import '@babel/polyfill'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+
+Vue.use(Vuetify)
 
 Vue.config.productionTip = false
 
@@ -11,6 +15,5 @@ new Vue({
   el: '#app',
   router,
   components: { App },
-  vuetify,
   template: '<App/>'
 })
