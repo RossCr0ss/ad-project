4246d0b412653df46d782864cca48920b94f0453
Rostyslav <sobechko@newveer.com>
2020-06-19 14:05:24 +0300

    add toolbar

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,43 @@
 <template>
   <v-app>
-    <v-navigation-drawer temporary app>
-      <!-- -->
+    <v-navigation-drawer 
+      temporary 
+      app 
+      v-model="drawer"
+    >
+      <v-list>
+        <v-list-tile 
+          :to="link.url"
+          v-for="link of links"
+          :key="link.title"
+        >
+          <v-list-tile-action>
+            <v-icon>{{link.icon}}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title v-text="link.title"></v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
     </v-navigation-drawer>
 
     <v-toolbar app dark color="primary">
-      <v-toolbar-side-icon></v-toolbar-side-icon>
-      <v-toolbar-title>Ad appcication</v-toolbar-title>
+      <v-toolbar-side-icon 
+        @click="drawer = !drawer"
+        class="hidden-md-and-up"
+      />
+      <v-toolbar-title>Ad application</v-toolbar-title>
       <v-spacer></v-spacer>
       <v-toolbar-items class="hidden-sm-and-down">
-          <v-btn flat>Link One</v-btn>
+        <v-btn
+          flat
+          v-for="link in links"
+          :key="link.title"
+          :to="link.url"
+        >
+          <v-icon left>{{link.icon}}</v-icon>
+          {{link.title}}
+        </v-btn>
       </v-toolbar-items>
     </v-toolbar>
 
@@ -30,7 +58,34 @@
 export default {
   data () {
     return {
-      //
+      drawer: false,
+      links: [
+        {
+          title: 'Login',
+          icon: 'lock',
+          url: '/login'
+        },
+        {
+          title: 'Registration',
+          icon: 'face',
+          url: '/registration'
+        },
+        {
+          title: 'Orders',
+          icon: 'bookmark_border',
+          url: '/orders'
+        },
+        {
+          title: 'New ad',
+          icon: 'note_add',
+          url: '/new'
+        },
+        {
+          title: 'My ads',
+          icon: 'list',
+          url: '/list'
+        }
+      ]
     }
   }
 }
