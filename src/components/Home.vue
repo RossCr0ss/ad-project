cdc0ad809c5c0dc40aaa28d90d2d31a51c3786e5
Rostyslav <sobechko@newveer.com>
2020-06-22 12:43:02 +0300

    ad cards at home page

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -9,15 +9,54 @@
               :key="i"
               :src="ad.imageSrc"
               transition="fade-transition"
-            ></v-carousel-item>
+            >
+            <div class="carousel-link">
+              <v-btn class="error" :to="'/ad/' + ad.id">{{ ad.title }}</v-btn>
+            </div>
+            </v-carousel-item>
           </v-carousel>
         </v-flex>
       </v-layout>
     </v-container>
-    <v-container>
-      <v-layout row>
-        <v-flex xs12>
-          <h1>Home</h1>
+    <v-container grid-list-lg>
+      <v-layout row wrap>
+        <v-flex 
+          xs12
+          sm6
+          md4
+          v-for="ad of ads"
+          :key="ad.id"
+        >
+          <v-card>
+            <v-card-media
+              :src="ad.imageSrc"
+              height="200px"
+            >
+            </v-card-media>
+
+            <v-card-title primary-title>
+              <div>
+                <h3 class="headline mb-0">{{ad.title}}</h3>
+                <div>{{ad.description}}</div>
+              </div>
+              Top western road trips
+            </v-card-title>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn
+                flat
+                :to="'/ad/' + ad.id"
+              >
+                Open
+              </v-btn>
+              <v-btn
+                raised
+                class="primary"
+              >
+                Buy
+              </v-btn>
+            </v-card-actions>
+          </v-card>
         </v-flex>
       </v-layout>
     </v-container>
@@ -62,3 +101,14 @@ export default {
   }
 }
 </script>
+<style scoped>
+  .carousel-link {
+    position: absolute;
+    bottom: 50px;
+    left: 50%;
+    background: rgba(0, 0, 0, 0.5);
+    transform: translate(-50%, 0);
+    padding: 5px 10px;
+    border-radius: 5px 5px 0 0;
+  }
+</style>
