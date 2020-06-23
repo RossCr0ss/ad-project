915b7ae688077f0de2806b1cac69be50dfbedd30
Rostyslav <sobechko@newveer.com>
2020-06-23 12:40:31 +0300

    done methods register and login

--- src/components/Auth/Login.vue
+++ src/components/Auth/Login.vue
@@ -36,7 +36,8 @@
             <v-btn
               color="primary"
               @click="onSubmit"
-              :disabled="!valid"
+              :loading="loading"
+              :disabled="!valid || loading"
             >Login</v-btn>
           </v-card-actions>
         </v-card>
@@ -62,6 +63,11 @@ export default {
       ]
     }
   },
+  computed: {
+    loading () {
+      return this.$store.getters.loading
+    }
+  },
   methods: {
     onSubmit () {
       if (this.$refs.form.validate()) {
@@ -69,7 +75,12 @@ export default {
           email: this.email,
           password: this.password
         }
-        console.log(user)
+        this.$store.dispatch('loginUser', user)
+          .then(() => {
+            this.$router.push('/')
+          })
+          .catch(err => console.log(err)
+          )
       }
     }
   }
